d979311b2d23762eb4e74f692ca2032eca9587cd
Igor Nikolaev <igor.sv.n@gmail.com>
2022-09-03 17:34:55 +0300

    Add dummy speak command.

--- src/Command/SpeakCommand.php
+++ src/Command/SpeakCommand.php
@@ -6,15 +6,21 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand('speak')]
 class SpeakCommand extends Command
 {
+    protected function configure(): void
+    {
+        $this->setDefinition([new InputArgument('speech', InputArgument::REQUIRED)]);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        exec('gtts-cli "Привет мир!" --lang ru | play -t mp3 - > /dev/null 2>&1');
+        exec(sprintf('gtts-cli "%s" --lang ru | play -t mp3 - > /dev/null 2>&1', (string) $input->getArgument('speech')));
 
         return Command::SUCCESS;
     }
