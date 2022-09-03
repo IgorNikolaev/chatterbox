155bdf3be17590445e32eaf038ef0d2fb06f36b0
Igor Nikolaev <igor.sv.n@gmail.com>
2022-09-03 17:58:49 +0300

    Use Symfony Dependency Injection.

--- src/Command/SpeakCommand.php
+++ src/Command/SpeakCommand.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Speech\Provider\SpeechProviderInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
@@ -15,15 +15,19 @@ use Symfony\Component\Process\Process;
 #[AsCommand('speak')]
 class SpeakCommand extends Command
 {
-    protected function configure(): void
+    private readonly SpeechProviderInterface $speechProvider;
+
+    public function __construct(SpeechProviderInterface $speechProvider)
     {
-        $this->setDefinition([new InputArgument('speech', InputArgument::REQUIRED)]);
+        parent::__construct();
+
+        $this->speechProvider = $speechProvider;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $process = Process::fromShellCommandline(
-            sprintf('gtts-cli "%s" --lang ru | play -t mp3 - > /dev/null 2>&1', $input->getArgument('speech'))
+            sprintf('gtts-cli "%s" --lang ru | play -t mp3 - > /dev/null 2>&1', $this->speechProvider->provideSpeech())
         );
         $process->run();
 
